855364d9dac5b83ab4fb64dced272e6f519ec5f8
ohse-emily <82620945+ohse-emily@users.noreply.github.com>
2021-07-06 14:59:29 +0900

    basic

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class App extends Component{
+    render(){
+        return(
+            <>
+                Hi ! I am basic webpack_settings 
+            </>
+        )
+    }
+}
+
+export default App 
